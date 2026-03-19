cb1902c6d08bfc463efa216ebb48cdf8c9536e1a
backup2iquilibrium <backup2.iquilibrium@gmail.com>
2026-03-19 18:29:06 -0300

    chore: remove temporary introspection and debug files.

--- introspect_chromium.cjs
+++
@@ -1,6 +0,0 @@
-const chromium = require('@sparticuz/chromium');
-console.log('Keys:', Object.keys(chromium));
-console.log('Default?', chromium.default ? Object.keys(chromium.default) : 'No default');
-console.log('Type of args:', typeof chromium.args);
-console.log('Type of defaultViewport:', typeof chromium.defaultViewport);
-console.log('Type of headless:', typeof chromium.headless);
